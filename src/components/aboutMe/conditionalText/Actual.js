00d96b9f215851a7300495cf3860f5b20e72ce45
Santiago Vittor <svittordev@gmail.com>
2024-10-09 15:35:21 -0300

    Actualizo CV y componente About me con informacion del 2024

--- src/components/aboutMe/conditionalText/Actual.js
+++ src/components/aboutMe/conditionalText/Actual.js
@@ -1,7 +1,7 @@
 import AOS from "aos";
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
-import CV from '../../../assets/pdfs/santiagoVittorResume.pdf'
+import CV from '../../../assets/pdfs/santiagoVittorResume2024.pdf'
 
 
 const ActualText = () => {
@@ -13,8 +13,8 @@ const ActualText = () => {
     return ( 
 
         <>
-            <p data-aos="fade-right">Mi nombre es Santiago Vittor, tengo 28 años y soy desarrollador frontend especializado en ReactJs.</p>
-            <p data-aos="fade-right">Actualmente realizo el módulo de programación backend en Coderhouse, en el que estoy aprendiendo a desenvolverme con NodeJs a través de su framework Express. También estoy adquiriendo nociones de bases de datos no relacionales y trabajando el enlace de mis aplicaciones con MongoDB.</p>
+            <p data-aos="fade-right">Mi nombre es Santiago Vittor, tengo 31 años y soy desarrollador frontend especializado en React.</p>
+            <p data-aos="fade-right">Actualmente estoy cursando la Tecnicatura en Sistemas de información con orientación en Desarrollo de Videojuegos en la Universidad Kennedy. También me desempeño como Squad Leader y DRQQA en la empresa FoodStyles. Me especializo en el desarrollo web con React, con un enfoque en la creación de soluciones eficientes y escalables. Como squad leader en Foodstyles, lidero equipos hacia la optimización de procesos y la entrega de productos de alta calidad, siempre manteniendo una colaboración fluida y enfocada en resultados.</p>
             <p>Puede descargar mi curriculum desde este enlace <span  id="cv"><a href={CV}>CV (159KB)</a></span></p>
         </>
 
--- src/components/aboutMe/conditionalText/Objetives.js
+++ src/components/aboutMe/conditionalText/Objetives.js
@@ -12,8 +12,8 @@ const Objetives = () => {
     return ( 
 
         <>
-        <p data-aos="fade-right" >Estoy en búsqueda de inspiración, sumando conocimiento y herramientas para plasmar mi mente en mis proyectos</p>
-        <p data-aos="fade-right">Entiendo que el aprendizaje es constante e ilimitado y busco con ansias mi primer oportunidad laboral como programador para poder trabajar junto a gente con experiencia que me ayude a crecer como profesional.</p>
+        <p data-aos="fade-right" >Estoy constantemente en busca de nuevas experiencias y sumando herramientas a mi set de skills.</p>
+        <p data-aos="fade-right">En mi carrera, me motiva seguir creciendo como líder y desarrollador, buscando continuamente perfeccionar mis habilidades en tecnologías emergentes. Mis próximos objetivos incluyen profundizar en arquitectura de software y cloud computing, con el propósito de construir aplicaciones aún más robustas y eficientes. Valoro el trabajo en equipo, la curiosidad por aprender, y me comprometo con la calidad en cada proyecto, siendo siempre fiel a mis principios de responsabilidad y lealtad</p>
         </>
 
      );
